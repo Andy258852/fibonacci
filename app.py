6c1dc0b18cca3801d9d27973bc369ffd7ecdc0f0
andry258852 <andry258852.gmail.com>
2021-03-08 19:10:38 +0300

    Major changes

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 import logging
+import math
 
 
-def set_config():
-    logging.basicConfig(format='%(message)s', level=logging.INFO)
-
-
-def fibonacci(num):
-    first = 0
-    second = 1
-    counter = 1
-    while counter < num:
-        first, second = second, second + first
-        counter += 1
-    return first
+def fibonacci(n):
+    sqrt_5 = math.sqrt(5)
+    phi = (sqrt_5 + 1) / 2
+    return int(phi ** n / sqrt_5 + 0.5)
 
 
 def main():
-    set_config()
-    logging.info('Input number of fibonacci sequence:')
-    try:
-        logging.info(fibonacci(int(input())))
-    except ValueError:
-        logging.info('Incorrect input')
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    num = input("Input number of fibonacci sequence: ")
+    if (num.isdigit()):
+        logging.info(fibonacci(int(num)))
+    else:
+        logging.info("Incorrect input")
 
 
 if __name__ == "__main__":
-    main()
+     try:
+         main()
+     except OverflowError:
+         logging.info("Number is too big")
+     except:
+         logging.info("Error")
